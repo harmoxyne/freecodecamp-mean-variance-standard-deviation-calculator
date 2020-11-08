9b3c0a08308c27f3960d1503885310e87fe4203d
harmoxyne <harmoxyne@gmail.com>
2020-11-08 20:16:41 +0200

    solution

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,18 @@
 import numpy as np
 
-def calculate(list):
 
+def calculate(pre_matrix):
+    if len(pre_matrix) < 9:
+        raise ValueError("List must contain nine numbers.")
+    matrix = np.array(pre_matrix).reshape(3, 3)
 
+    result = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var()],
+        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum()]
+    }
 
-
-    return calculations
+    return result
